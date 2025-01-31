9d458e769fc8d1005b2562abc9102ba45bd20db3
lab396 <50845356+lab396@users.noreply.github.com>
2025-01-31 15:36:07 -0500

    Update transfer.py

--- automation_scripts/transfer.py
+++ automation_scripts/transfer.py
@@ -1,78 +1,65 @@
 #!/usr/bin/env python
 
- 
-
-# This helper script will transfer the zip data and manifest files
-# from the CQI working scratch directory to the appropriate archive
-# directoryusing Globus and persistent authentication using
-# a Globus refresh token. 
+# This script will list the contents of a user-defined Globus Guest Collection,
+# using Globus and persistent authentication using a refresh token. 
 #
-# This makes use of the Globus-hosted native app "MyGroup Archive_Tranfer".
+# This makes use of the Globus-hosted native app "MRI_2_RC_Test_App".
 # 
-# Upon initial run it will open a web browser for initial authentication,
+# The first time this app is run, it will open a web browser for initial authentication,
 # after which a token file will be saved in the $CWD. Tokens can be 
 # recinded by deleting the file or going to 
 # https://app.globus.org/settings/consents, referencing the consent, and
 # then deleting it.
 #
-# It requires two command line arguments to function:
-#    The $LOCATION variable, that specifies which CQI location src and dest
-#    The $today variable, that specifies the date of the archive run and
-#      location directory 
+# Written by Lindsay Wells adapted from J. Nucciarone and Globus support
+# January 23, 2025.
 #
-# Written by J. Nucciarone with assistance from Globus support
-# April 29, 2024.
+# Version 1.0
 #
-#  Version 1.0
-#
-
-
 import os
 
+import time
 
 import argparse
 
 import webbrowser
-
 
 import globus_sdk
 from globus_sdk import GroupsClient, NativeAppAuthClient, RefreshTokenAuthorizer
 from globus_sdk.tokenstorage import SimpleJSONFileAdapter
 from globus_sdk.scopes import TransferScopes
 
- 
-
 # Obtain command line args that contain the location and date info
-parser = argparse.ArgumentParser()
-parser.add_argument("DATA_LOCATION")
-parser.add_argument("TODAY")
-args = parser.parse_args()
-
+# parser = argparse.ArgumentParser()
+# parser.add_argument("DATA_LOCATION")
+# parser.add_argument("TODAY")
+# args = parser.parse_args()
  
-
 # Set source and destination endpoint UUID
-# Source is the Guest Collection "MyGroup Active Transfer Directory"
-# This guest collection corrsponds to directory /storage/group/xyz123/default/INPUT
+# Source is the Guest Collection "MRI_Converge_Guest_Collection"
+# This guest collection corrsponds to directory /storage/long/ on converge.mri.psu.edu
 
-source_collection_id = "a7b0d0fe-f0ef-4186-a96b-fc89ee61679a" 
+source_collection_id = "095bd11b-e263-44dc-ad19-6dd4b463207e" 
 
-# Destination is the Guest Collection "MyGroup Archive Transfer Directory"
-# This guest collection corresponds to directory /storage/archive/xyz123/default/OUTPUT
+# Destination is the Guest Collection "MRI_Data_Transfer_Guest_Collection"
+# This guest collection corresponds to directory /storage/group/MCL/default/globus-share/
 
-dest_collection_id = "e3a52e0a-b824-4d4b-9b04-1dad86e54c07"
+# dest_collection_id = "dbbff0bc-6329-45b1-9bfe-50624cc42ce6"
 
-# My client ID - "MyGroup Archive Transfer" native app
+dest_collection_id = "eef905af-dc3e-4f55-8c87-7b804cfb654f"
 
-CLIENT_ID = "a619acea-bc58-40c7-ad6a-fd422c75bfd0"
+# My client ID - "MRI_2_RC_Test_App" native app
 
- 
+CLIENT_ID = "0ba8e7a7-3f08-49bd-adde-61491161882c"
+
+# Set the source and destination paths
+source_path = '/lab396'
+destination_path = '/'
 
 auth_client = NativeAppAuthClient(CLIENT_ID)
 my_file_adapter = SimpleJSONFileAdapter(
-    os.path.expanduser("/storage/home/xyz123/mytokens/my-svc-acct-globus-groups-tokens.json")
+    os.path.expanduser("/root/globus_auth_scripts/my-globus-refresh-token.json")
 )
-
- 
 
 if not my_file_adapter.file_exists():
     # requested_scopes specifies a list of scopes to request
@@ -82,13 +69,11 @@ if not my_file_adapter.file_exists():
     authorize_url = auth_client.oauth2_get_authorize_url()
      
     print(f"Native App Authorizaion URL:\n{authorize_url}\n")
-
-
+    
     try:
         webbrowser.open(authorize_url, new=1)
     except:
         print(f"Please go to this URL and login:\n\n{authorize_url}\n")
-
 
     auth_code = input("Please enter the code here: ").strip()
 
@@ -113,19 +98,51 @@ authorizer = globus_sdk.RefreshTokenAuthorizer(
     on_refresh=my_file_adapter.on_refresh,
 )
 
- 
-
 # construct an AccessTokenAuthorizer and use it to construct the
 # TransferClient
 
-
 transfer_client = globus_sdk.TransferClient(authorizer=authorizer)
- 
 
 # create a Transfer task consisting of one or more items
 task_data = globus_sdk.TransferData(
     transfer_client,source_endpoint=source_collection_id, destination_endpoint=dest_collection_id
 )
+
+# Create a transfer data object
+transfer_data = globus_sdk.TransferData(
+    transfer_client,
+    source_collection_id,
+    dest_collection_id,
+    label="Transfer from /storage/long to /storage/group/MCL/default/globus-share",
+    sync_level="checksum",  # Use "checksum" for data validation
+)
+
+# Add file paths to the transfer data object
+transfer_data.add_item(source_path, destination_path)
+
+task_data.add_filter_rule(name="*", method="include", type="dir")
+
+# Start the transfer
+print("Starting transfer...")
+transfer_result = transfer_client.submit_transfer(transfer_data)
+
+# Monitor the transfer status
+transfer_id = transfer_result['task_id']
+print(f"Transfer started with task ID: {transfer_id}")
+
+while True:
+    task = transfer_client.get_task(transfer_id)
+    status = task['status']
+    
+    if status == 'SUCCEEDED':
+        print(f"Transfer completed successfully.")
+        break
+    elif status == 'FAILED':
+        print(f"Transfer failed. Check Globus dashboard for details.")
+        break
+    
+    print(f"Transfer status: {status}. Waiting 5 seconds...")
+    time.sleep(5)
 
 
 # Set the source and destination transfers, and build filters to
@@ -134,30 +151,29 @@ task_data = globus_sdk.TransferData(
 # the DATA directory.
 #
 
-task_data.add_item(
-    f"./{args.DATA_LOCATION}/{args.TODAY}/",  # Source
-    f"./{args.DATA_LOCATION}/DATA/",  # Dest
-    recursive=True
-)
+# task_data.add_item(
+#    f"./{args.DATA_LOCATION}/{args.TODAY}/",  # Source
+#    f"./{args.DATA_LOCATION}/DATA/",  # Dest
+#    recursive=True
+#)
 
 
 # Rules set following directions at 
 # https://docs.globus.org/api/transfer/task_submit/#filter_rules
 
 
-task_data.add_filter_rule(name="*.zip", method="include", type="file")
-task_data.add_filter_rule(name="MANIFESTS", method="exclude", type="dir")
-task_data.add_filter_rule(name=f"eligible_for_deletion_{args.TODAY}.txt", method="exclude", type="file")
+# task_data.add_filter_rule(name="*", method="include", type="dir")
+# task_data.add_filter_rule(name="MANIFESTS", method="exclude", type="dir")
+# task_data.add_filter_rule(name=f"eligible_for_deletion_{args.TODAY}.txt", method="exclude", type="file")
  
 
 # Now all the MANIFESTS directory back and transfer it to its own location
 
-task_data.add_item(
-    f"./{args.DATA_LOCATION}/{args.TODAY}/MANIFESTS/",  # Source
-    f"./{args.DATA_LOCATION}/MANIFESTS/",  # Dest
-    recursive=True
-)
-
+# task_data.add_item(
+#    f"./{args.DATA_LOCATION}/{args.TODAY}/MANIFESTS/",  # Source
+#    f"./{args.DATA_LOCATION}/MANIFESTS/",  # Dest
+#    recursive=True
+#)
 
 task_data.add_filter_rule(name="*.txt", method="include", type="file")
 
